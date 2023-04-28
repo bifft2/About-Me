b043db9607a0207f701143c42e7a261c9b0a94da
bifft2 <bifft95@gmail.com>
2023-04-28 16:45:51 -0500

    adding About Me app start

--- About Me/ContentView.swift
+++ About Me/ContentView.swift
@@ -11,17 +11,25 @@ struct ContentView: View {
     @State private var showingAlert = false
     var body: some View {
         ZStack {
-            Color(.systemPurple)
+            Color(.systemMint)
                             .ignoresSafeArea()
-            VStack {
+            VStack(alignment: .center, spacing: 20.0) {
                 Image("cat")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .padding(.horizontal)
                 Text("Brianna")
+                    .font(.title)
                 Button("Learn More!") {
                     showingAlert = true
                 }
+                .padding(.all, 5.0)
                 .alert("Brianna's cat's name is Forrest!", isPresented: $showingAlert) {
                             Button("OK", role: .cancel) { }
-                        }
+                }
+                .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color.white/*@END_MENU_TOKEN@*/)
+                .buttonBorderShape(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=shape: ButtonBorderShape@*/.capsule/*@END_MENU_TOKEN@*/)
+                .border(/*@START_MENU_TOKEN@*/Color.gray/*@END_MENU_TOKEN@*/, width: /*@START_MENU_TOKEN@*/1/*@END_MENU_TOKEN@*/)
             }
         }
     }
